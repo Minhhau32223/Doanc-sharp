0d3570511048007eea8b8afab019039bfb63a4e9
Vương <vuongledz04@gmail.com>
2025-04-18 16:49:50 +0700

    add LSHoatDongBUS

---
+++ Doanc-sharp/src/BUS/LSHoatDongBUS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Doanc_sharp.src.DAO;
+using Doanc_sharp.src.DTO;
+using System.Data;
+
+namespace Doanc_sharp.src.BUS
+{
+    internal class LSHoatDongBUS
+    {
+        private LSHoatDongDAO lsHoatDongDAO = new LSHoatDongDAO();
+
+        public DataTable GetAllLSHoatDong()
+        {
+            return lsHoatDongDAO.GetAllLSHoatDong();
+        }
+
+        public LSHoatDongDTO GetLSHoatDongById(int maThanhVien, int maHoatDong)
+        {
+            return lsHoatDongDAO.GetLSHoatDongById(maThanhVien, maHoatDong);
+        }
+
+        public Boolean InsertLSHoatDong(LSHoatDongDTO lsHoatDong)
+        {
+            return lsHoatDongDAO.InsertLSHoatDong(lsHoatDong) > 0;
+        }
+
+        public Boolean UpdateLSHoatDong(LSHoatDongDTO lsHoatDong)
+        {
+            return lsHoatDongDAO.UpdateLSHoatDong(lsHoatDong) > 0;
+        }
+
+        public Boolean DeleteLSHoatDong(int maThanhVien, int maHoatDong)
+        {
+            return lsHoatDongDAO.DeleteLSHoatDong(maThanhVien, maHoatDong) > 0;
+        }
+    }
+}
