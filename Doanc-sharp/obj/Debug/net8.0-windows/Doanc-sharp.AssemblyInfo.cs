9ae628555a9701ffe51f8f8db03a342f0bd4fd1e
MyPhungg <myphunglop71@gmail.com>
2025-04-20 09:32:04 +0700

    danh muc

--- Doanc-sharp/obj/Debug/net8.0-windows/Doanc-sharp.AssemblyInfo.cs
+++ Doanc-sharp/obj/Debug/net8.0-windows/Doanc-sharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Doanc-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+957a64639dbbbc4884914123d4eedc5d607e7083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e93e83770b50928f0b8cef400cf31038128e5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Doanc-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Doanc-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
