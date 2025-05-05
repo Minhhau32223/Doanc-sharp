d161c7459d99d0990a40a89d2dfbdd51b080e079
Minhhau32223 <hau334779@gmail.com>
2025-05-05 22:29:41 +0700

    add gitignore

--- Doanc-sharp/obj/Debug/net8.0-windows/Doanc-sharp.AssemblyInfo.cs
+++ Doanc-sharp/obj/Debug/net8.0-windows/Doanc-sharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Doanc-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf113c75827d2828816dc3c7c6f99f2a82a14b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba04aaed661025c9a3845218db18719fc21733c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Doanc-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Doanc-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
